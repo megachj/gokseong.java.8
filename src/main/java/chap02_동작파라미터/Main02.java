7686b364aa3f0c649ef7bd9440c0bd87837968f3
최해진/게임플랫폼서버팀 <megachj@nhnent.cm>
2019-02-26 18:27:20 +0900

    서브모듈 추가
    - gokseong.java.effective 서브모듈

--- src/main/java/chap02_동작파라미터/Main02.java
+++ src/main/java/chap02_동작파라미터/Main02.java
@@ -1,9 +1,12 @@
 package chap02_동작파라미터;
 
+import chap30_enum정의.classes.Person;
+
 public class Main02 {
     public static void main(String[] args) {
         System.out.println("Main02 start");
 
-
+        Person person = new Person("choe", 19);
+        System.out.println(person.getName());
     }
 }
